df135b4766057c08ea77e887e1ac8c4fec5aa4bb
MinhQuang28 <86767785+Quangmightyjaxx@users.noreply.github.com>
2022-05-17 21:27:44 +0700

    fix route api

--- src/route/v1/api.ts
+++ src/route/v1/api.ts
@@ -1,22 +1,22 @@
 import express from 'express';
 const router = express.Router();
 import { validate } from '../../middlewares/validate';
-import { companyController } from '../../controller';
+import { companyController, branchController, storeController, foodController, GroupOptionFoodController, optionFoodController } from '../../controller';
 import { companyValidation, verifyPageLimit, storeValidation, branchValidation } from '../../validations';
 // list route Company
 router.post('/company', validate(companyValidation.createCompany), companyController.createCompany);
 router.get('/company', validate(verifyPageLimit), companyController.getListCompany);
 //list route branch
-router.post('/branch', validate(branchValidation.createBranch), companyController.createCompany);
-router.get('/branch', validate(verifyPageLimit), companyController.getListCompany);
+router.post('/branch', validate(branchValidation.createBranch), branchController.createBranch);
+router.get('/branch', validate(verifyPageLimit), branchController.getListBranches);
 //list route store
-router.post('/store', validate(storeValidation.createStore), companyController.createCompany);
-router.get('/store', validate(verifyPageLimit), companyController.getListCompany);
+router.post('/store', validate(storeValidation.createStore), storeController.createStore);
+router.get('/store', validate(verifyPageLimit), storeController.getListStore);
 //list route food
-router.post('/food', validate(storeValidation.createStore), companyController.createCompany);
-router.get('/food', validate(verifyPageLimit), companyController.getListCompany);
+router.post('/food', validate(storeValidation.createStore), foodController.createFood);
+router.get('/food', validate(verifyPageLimit), foodController.getListFoods);
 //list route group option food
-router.post('/group-option', validate(storeValidation.createStore), companyController.createCompany);
+router.post('/group-option', validate(storeValidation.createStore), GroupOptionFoodController.createGroupOptionFood);
 //list route  option food
-router.post('/option', validate(storeValidation.createStore), companyController.createCompany);
+router.post('/option', validate(storeValidation.createStore), optionFoodController.createOptionFood);
 export const apiRouteV1 = router;
